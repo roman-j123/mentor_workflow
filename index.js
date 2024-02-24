a3ae8454b303869eb003296ad413ca1c17cb1708
Roman Ismagilov <39709476+roman-j123@users.noreply.github.com>
2024-02-24 17:30:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const generateArray = (len, symb = '') => {
   return result;
 }
 
-//Напиши функцию, которая разворачивает массив в обратном порядке.
-//В аргумент функции передается массив [1, 2, 3] должны вернуть  [3, 2, 1]
+// Напиши функцию, которая разворачивает массив в обратном порядке.
+// В аргумент функции передается массив [1, 2, 3] должны вернуть  [3, 2, 1]
 
 const reverseArray = (array = []) => {
   const result = [];
@@ -24,6 +24,24 @@ const reverseArray = (array = []) => {
   }
   for (let i = array.length - 1; i >= 0; i--) {
     result.push(array[i])
+  }
+  return result;
+}
+
+// Напиши функцию, которая очищает массив от нежелательных значений, таких как false, undefined, пустые строки, ноль, null.
+// Ожидаемый результат: [0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+
+const filterArray = (array = []) => {
+  const result = [];
+  if (array.length === 0) {
+    console.log('Ошибка, Вы не задали массив');
+    return;
+  }
+  for (const item of array) {
+    const numItem = Number(item);
+    if (numItem > 0) { // <- Тут конечно вопросик про приведение к типу, если оставить условие numItem !== NaN - NaN попадает в массив
+      result.push(numItem)
+    }
   }
   return result;
 }
