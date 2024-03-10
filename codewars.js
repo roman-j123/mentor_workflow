0d801581a3dc9a0940541c72ed22b101475ee29e
Roman Ismagilov <39709476+roman-j123@users.noreply.github.com>
2024-03-10 12:03:51 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -12,3 +12,22 @@ const uniqueInOrder = (value) => {
   }
   return result;
 }
+
+// [6kyu]
+// Digital root is the recursive sum of all the digits in a number. 
+// Given n, take the sum of the digits of n. 
+// If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+// The input will be a non-negative integer.
+//    16  -->  1 + 6 = 7
+//   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+const digitalRoot = (num) => {
+  let strNum = String(num);
+  let result = 0;
+  if (strNum.length === 1) return strNum;
+  for (const i of strNum) {
+    result += Number(i);
+  }
+  return digitalRoot(result);
+}
